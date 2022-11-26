e36eb639e962a930d588062d562c41fa81495d0c
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-26 19:43:16 +0900

    add: ヘッダーとメインのレイアウト

--- pages/components/layout/Layout.js
+++ pages/components/layout/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../header/header";
+import styles from "./Layout.module.css";
 
 function Layout({ children }) {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <Header />
       </header>
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </>
   );
 }
