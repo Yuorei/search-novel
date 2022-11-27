0c056fd3f14b52a26fb07d2bebad73c6f0f748d7
Yuorei <yuorei71@gmail.com>
2022-11-27 11:40:40 +0900

    db起動

--- back/sever.js
+++ back/sever.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import 'dotenv/config'; // このモジュールで.envから環境変数を設定する
+import {dbInit} from './db.js'
+
 const port = process.env.PORT;
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+dbInit()
 app.get('/', (req, res) => {
   res.json([
     { message: 'Hello, World' },
   ]);
 });
 
+//サーバー起動
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
