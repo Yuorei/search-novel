e7442e4f07364d5e9accddcffc5c026e1dce6f21
Yuorei <yuorei71@gmail.com>
2022-11-27 12:58:27 +0900

    id返す

--- back/login.js
+++ back/login.js
@@ -1,8 +1,10 @@
 import{dbSerectUser} from 'db.js'
-function login(){
-    const id=dbSerectUser()
+function login(mail,password){
+    var id=dbSerectUser(mail,password)
     if(id!=null){
-
+        return id;
     }
-    
+    //間違っている場合は0とする
+    return 0;
 }
+export {login};
