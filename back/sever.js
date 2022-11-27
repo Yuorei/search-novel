d7a2da5547096b535b5553572e411f466dd149bd
Yuorei <yuorei71@gmail.com>
2022-11-27 14:13:34 +0900

    user登録

--- back/sever.js
+++ back/sever.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import 'dotenv/config'; // このモジュールで.envから環境変数を設定する
 import {dbInit} from './db.js'
-import {login} from './login.js'
+import {login,register} from './login.js'
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 dbInit()
+
 app.post('/login', (req, res) => {
   var id=login(req.body.mail,req.body.password)
   res.json({ id: id });
+});
+
+app.post('/register', (req, res) => {
+  register(req.body.mail,req.body.password)
+  res.json({mail: req.body.mail ,password:req.body.password});
 });
 
 //サーバー起動
