a6ad08d12261b3a6edc9120a2f6a95008883c49f
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-27 15:50:08 +0900

    fix: Headerへのパス修正

--- pages/components/layout/Layout.js
+++ pages/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../header/header";
+import Header from "../header/Header";
 import styles from "./Layout.module.css";
 
 function Layout({ children }) {
